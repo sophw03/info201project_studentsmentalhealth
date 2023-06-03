848a31980830c3717468eb2d1f6e8a19b15b3179
leilaniflores <129806012+leilaniflores@users.noreply.github.com>
2023-06-03 00:27:34 -0700

    Update FinalProject_ShinyCode.R

--- FinalProject_ShinyCode.R
+++ FinalProject_ShinyCode.R
@@ -1,9 +1,7 @@
 # Shiny Code for Final Project
-
 # Libraries
 library(shiny)
 library(ggplot2)
-
 
 # Source the final data frame
 source("final.R")
@@ -34,10 +32,20 @@ ui <- fluidPage(
                )
              )
     ),
+    
     # Story 2
     tabPanel("The Impact of Mental Health on College Students Academic Performance",
-             h3("Story 2"),
-             p("Describe the limitations and problems of the study.")
+             sidebarLayout(
+               sidebarPanel(
+                 h4("Navigation"),
+                 br(),
+                 h5("Select Graph Type:"),
+                 radioButtons("story2_graph_type", label = NULL, choices = c("Depression", "Panic Attacks", "Anxiety"), selected = "Depression")
+               ),
+               mainPanel(
+                 plotOutput("story2_plot")
+               )
+             )
     ),
     
     # Story 3
@@ -47,7 +55,7 @@ ui <- fluidPage(
                  h4("Navigation"),
                  br(),
                  h5("Select Graph:"),
-                 radioButtons("graph_type", label = NULL, choices = c("Depression", "Anxiety", "Panic Attacks"), selected = "Depression")
+                 radioButtons("story3_graph_type", label = NULL, choices = c("Depression", "Anxiety", "Panic Attacks"), selected = "Depression")
                ),
                mainPanel(
                  plotOutput("selected_plot")
@@ -69,6 +77,7 @@ ui <- fluidPage(
     )
   )
 )
+
 # Server
 server <- function(input, output) {
   
@@ -107,22 +116,49 @@ server <- function(input, output) {
     }
   })
   
+  # Render the anxiety by age plot
+  output$story2_plot <- renderPlot({
+    if (input$story2_graph_type == "Depression") {
+      # Visualization for data for GPA
+      ggplot(summary_data_gpa, aes(x = GPA_Range, y = Percentage_Depression)) +
+        geom_bar(stat = "identity", fill = "darkgreen") +
+        labs(title = "Percentage of People with Depression by GPA Range",
+             x = "GPA Range",
+             y = "Percentage of People with Depression")
+    } else if (input$story2_graph_type == "Panic Attacks") {
+      # Visualization for panic attacks by GPA
+      ggplot(summary_data_gpa, aes(x = GPA_Range, y = Percentage_Panic_Attacks)) +
+        geom_bar(stat = "identity", fill = "darkorange") +
+        labs(title = "Percentage of People with Panic Attacks by GPA Range",
+             x = "GPA Range",
+             y = "Percentage of People with Panic Attacks")
+    } else if (input$story2_graph_type == "Anxiety") {
+      # Visualization for anxiety by GPA
+      ggplot(summary_data_gpa, aes(x = GPA_Range, y = Percentage_Anxiety)) +
+        geom_bar(stat = "identity", fill = "darkblue") +
+        labs(title = "Percentage of People with Anxiety by GPA Range",
+             x = "GPA Range",
+             y = "Percentage of People with Anxiety")
+    }
+  })
+  
+  # Render the selected plot based on the user's choice in Story 3
   output$selected_plot <- renderPlot({
-    if (input$graph_type == "Depression") {
+    if (input$story3_graph_type == "Depression") {
       # Visualization for depression by major
       ggplot(summary_data_major, aes(x = Major, y = Percentage_Depression)) +
         geom_bar(stat = "identity", fill = "lightpink") +
         labs(title = "Percentage of People with Depression by Major",
              x = "Major",
              y = "Percentage of People with Depression")
-    } else if (input$graph_type == "Anxiety") {
+    } else if (input$story3_graph_type == "Anxiety") {
       # Visualization for anxiety by major
       ggplot(summary_data_major, aes(x = Major, y = Percentage_Anxiety)) +
         geom_bar(stat = "identity", fill = "lightblue") +
         labs(title = "Percentage of People with Anxiety by Major",
              x = "Major",
              y = "Percentage of People with Anxiety")
-    } else if (input$graph_type == "Panic Attacks") {
+    } else if (input$story3_graph_type == "Panic Attacks") {
       # Visualization for panic attacks by major
       ggplot(summary_data_major, aes(x = Major, y = Percentage_Panic_Attacks)) +
         geom_bar(stat = "identity", fill = "lightgreen") +
